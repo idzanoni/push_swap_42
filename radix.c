51ca6d47621588c31454396cf84890e052e45f6d
Izabel Zanoni <izanoni@c1r7p4.42sp.org.br>
2024-01-10 15:41:06 -0300

    .

--- radix.c
+++ radix.c
@@ -1,31 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   radix.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: izanoni <izanoni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/03 15:37:21 by izanoni           #+#    #+#             */
-/*   Updated: 2023/05/25 21:52:00 by izanoni          ###   ########.fr       */
+/*   Created: 2024/01/10 15:40:27 by izanoni           #+#    #+#             */
+/*   Updated: 2024/01/10 15:40:51 by izanoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && n > 0)
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
-		n--;
-	}
-	if (n == 0)
-		return (0);
-	else
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
+#include "push_swap.h"
